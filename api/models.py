c7e1f2754a7046330a161d20bc2cbd84f7afedbe
Mansur850 <mansurnauryzbaj8@gmail.com>
2025-02-24 14:57:50 +0500

    Add files via upload

---
+++ api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from datetime import datetime
+
+class Item(models.Model):
+    content = models.TextField()
+    is_finished = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return f"item id:{self.pk}"
+# Create your models here.
